a2f9163e9317e4f4ed44bd507900d2c721f57811
Azryel <AzryelShroud@gmail.com>
2021-07-12 22:08:57 +0200

    show connect and disconnect messages

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+
+    socket.broadcast.emit('connect-disconnect', "A user connected");
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
-        console.log('message: ' + msg);
       });
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        socket.broadcast.emit('connect-disconnect', "A user disconnected");
     });
   });
   
