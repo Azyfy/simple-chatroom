0f7b5db7079a1251c1d54a3a24ed673dac21718b
Azryel <AzryelShroud@gmail.com>
2021-07-13 21:27:33 +0200

    add username

--- index.js
+++ index.js
@@ -11,16 +11,36 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+const users = []; 
 
-    socket.broadcast.emit('connect-disconnect', "A user connected");
+io.on('connection', (socket) => {
+    
+    socket.broadcast.emit('connect-disconnect', `A user connected.`);
+    socket.emit('connect-disconnect',  `${users.length} user(s) in the chatroom.`);
+    io.emit("users", users);
+
+    socket.on('username', (username) => {
+      if(users.includes(username)) {
+        console.log("username taken") //handled on client, shouldnt happen
+        return;
+      }
+      socket.username = username;
+
+      users.push( {username, id: socket.id} );
+      socket.emit('connect-disconnect', `Welcome ${socket.username}`);
+      socket.broadcast.emit('connect-disconnect', `Say hi to ${socket.username}`);
+    });
 
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        io.emit('chat message', socket.username, msg);
       });
 
     socket.on('disconnect', () => {
-        socket.broadcast.emit('connect-disconnect', "A user disconnected");
+      
+      const index = users.findIndex( user => user.id === socket.id  );
+      if(index > -1) { users.splice(index, 1); }
+
+      socket.broadcast.emit('connect-disconnect', `${socket.username} disconnected. ${users.length} user(s) in the chatroom.`);
     });
   });
   
